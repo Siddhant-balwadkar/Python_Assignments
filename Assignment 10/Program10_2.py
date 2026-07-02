3aefae72bb95580b6714e36344d5e88ab7f02624
Siddhant-balwadkar <siddhantvbalwadkar@gmail.com>
2026-07-02 17:29:58 +0530

    Update author date in Program10_2.py
    Updated author date in comments and added newline at end of file.

--- Assignment 10/Program10_2.py
+++ Assignment 10/Program10_2.py
@@ -9,7 +9,7 @@
 #  Function Name :     SumNaturalNum
 #  Description :       sum of first N natural numbers
 #  Author :            Siddhant Vikas Balwadkar
-#  Date :              01/01/2026
+#  Date :              01/07/2026
 #
 ##############################################################
 def SumNaturalNum(No):
@@ -26,7 +26,7 @@ def SumNaturalNum(No):
 #  Function Name :     main
 #  Description :       It is the main function 
 #  Author :            Siddhant Vikas Balwadkar
-#  Date :              01/01/2026
+#  Date :              01/07/2026
 #
 ##############################################################
 def main():
